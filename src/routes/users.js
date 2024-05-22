8109ec76eb2394c180659257f740b6203aa27cf9
SideeID <sandipratama164026@gmail.com>
2024-05-22 15:47:43 +0700

    chore: Add authentication middleware and profile endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,12 +4,16 @@ const {
   loginUser,
   getUserById,
   updateUser,
+  getOwnProfile,
 } = require('../controllers/userController');
+
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/profile', authMiddleware, getOwnProfile);
 router.get('/:id', getUserById);
 router.put('/:id', updateUser);
 
