5cda132c81207ab15e68fb34d4878cbb74fe2cab
SideeID <sandipratama164026@gmail.com>
2024-05-23 18:54:22 +0700

    fix: update fundraiser endpoint

--- src/controllers/fundraiserController.js
+++ src/controllers/fundraiserController.js
@@ -24,21 +24,17 @@ const createFundraiser = async (req, res) => {
     });
     await fundraiser.save();
 
-    return res
-      .status(201)
-      .json({
-        success: true,
-        message: 'Penggalangan dana berhasil dibuat',
-        fundraiser,
-      });
+    return res.status(201).json({
+      success: true,
+      message: 'Penggalangan dana berhasil dibuat',
+      fundraiser,
+    });
   } catch (error) {
-    return res
-      .status(500)
-      .json({
-        success: false,
-        message: 'Terjadi kesalahan pada server',
-        error: error.message,
-      });
+    return res.status(500).json({
+      success: false,
+      message: 'Terjadi kesalahan pada server',
+      error: error.message,
+    });
   }
 };
 
@@ -49,13 +45,11 @@ const getAllFundraisers = async (req, res) => {
       .exec();
     return res.status(200).json({ success: true, fundraisers });
   } catch (error) {
-    return res
-      .status(500)
-      .json({
-        success: false,
-        message: 'Terjadi kesalahan pada server',
-        error: error.message,
-      });
+    return res.status(500).json({
+      success: false,
+      message: 'Terjadi kesalahan pada server',
+      error: error.message,
+    });
   }
 };
 
@@ -74,13 +68,85 @@ const getFundraiserById = async (req, res) => {
 
     return res.status(200).json({ success: true, fundraiser });
   } catch (error) {
-    return res
-      .status(500)
-      .json({
+    return res.status(500).json({
+      success: false,
+      message: 'Terjadi kesalahan pada server',
+      error: error.message,
+    });
+  }
+};
+
+// edit fundraiser
+const updateFundraiser = async (req, res) => {
+  const { id } = req.params;
+  const {
+    title, description, image, isClosed, endDate, goal,
+  } = req.body;
+
+  if (!title && !description && !image && !isClosed && !endDate && !goal) {
+    return res.status(400).json({
+      success: false,
+      message: 'Harap isi setidaknya satu bidang',
+    });
+  }
+
+  try {
+    const fundraiser = await Fundraiser.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        image,
+        isClosed,
+        endDate,
+        goal,
+      },
+      { new: true },
+    );
+
+    if (!fundraiser) {
+      return res.status(404).json({
         success: false,
-        message: 'Terjadi kesalahan pada server',
-        error: error.message,
+        message: 'Penggalangan dana tidak ditemukan',
       });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: 'Penggalangan dana berhasil diperbarui',
+      fundraiser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: 'Terjadi kesalahan pada server',
+      error: error.message,
+    });
+  }
+};
+
+const deleteFundraiser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const fundraiser = await Fundraiser.findByIdAndDelete(id);
+    if (!fundraiser) {
+      return res.status(404).json({
+        success: false,
+        message: 'Penggalangan dana tidak ditemukan',
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: 'Penggalangan dana berhasil dihapus',
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: 'Terjadi kesalahan pada server',
+      error: error.message,
+    });
   }
 };
 
@@ -88,4 +154,6 @@ module.exports = {
   createFundraiser,
   getAllFundraisers,
   getFundraiserById,
+  updateFundraiser,
+  deleteFundraiser,
 };
